99b044aad34825b5948378e57125caf1bebabf7e
willior <31899430+willior@users.noreply.github.com>
2020-08-20 18:30:17 -0400

    player hitbox created, grass animation working

--- assets/World/Grass.gd
+++ assets/World/Grass.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
-func _process(delta):
-	if Input.is_action_just_pressed("attack"):
-		var GrassEffect = load("res://assets/Effects/GrassEffect.tscn") # load the scene and store it in a variable
-		var grassEffect = GrassEffect.instance() # instance a packed scene of GrassEffect, storing it inside of its own variable, node grassEffect
-		var world = get_tree().current_scene # getting the very first scene, in this case World
-		world.add_child(grassEffect) # adding the grassEffect scene to the World scene
-		grassEffect.global_position = global_position # global_position is the position of Grass; sets the position of the grassEffect to the position of Grass
-		queue_free()
+func create_grass_effect():
+
+	var GrassEffect = load("res://assets/Effects/GrassEffect.tscn") # load the scene and store it in a variable
+	var grassEffect = GrassEffect.instance() # instance a packed scene of GrassEffect, storing it inside of its own variable, node grassEffect
+	var world = get_tree().current_scene # getting the very first scene, in this case World
+	world.add_child(grassEffect) # adding the grassEffect scene to the World scene
+	grassEffect.global_position = global_position # global_position is the position of Grass; sets the position of the grassEffect to the position of Grass
+
+func _on_Hurtbox_area_entered(area):
+	create_grass_effect()
+	queue_free()
