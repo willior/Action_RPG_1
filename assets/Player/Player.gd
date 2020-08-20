4d3f62106fef16f1d957935bf5cd4b593318d719
willior <31899430+willior@users.noreply.github.com>
2020-08-20 13:49:09 -0400

    Initial commit

---
+++ assets/Player/Player.gd
@@ -0,0 +1,29 @@
+extends KinematicBody2D
+
+const ACCELERATION = 1000
+const MAX_SPEED = 160
+const FRICTION = 1000
+
+var velocity = Vector2.ZERO
+
+onready var animationPlayer = $AnimationPlayer # declaring animationPlayer to give access to the AnimationPlayer node
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength ("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		if input_vector.x > 0:
+			animationPlayer.play("RunRight")
+		else:
+			animationPlayer.play("RunLeft")
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	else:
+		animationPlayer.play("IdleRight")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		
+	print(velocity)
+	
+	velocity = move_and_slide(velocity)
