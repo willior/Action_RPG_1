5a98c7cbde6a5d8744bb591abf314d37578ab027
willior <31899430+willior@users.noreply.github.com>
2020-08-20 17:55:13 -0400

    working on grass animation

--- assets/World/Grass.gd
+++ assets/World/Grass.gd
@@ -2,4 +2,8 @@ extends Node2D
 
 func _process(delta):
 	if Input.is_action_just_pressed("attack"):
+		var GrassEffect = load("res://assets/Effects/GrassEffect.tscn")
+		var grassEffect = GrassEffect.instance() # creating a packed scene of GrassEffect, storing it inside of node grassEffect
+		var world = get_tree().current_scene # getting the very first scene, in this case World
+		world.add_child(grassEffect)
 		queue_free()
