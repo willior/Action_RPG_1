cec032a0d020d8c7e7853bbf45c3d2d266ba57dc
willior <31899430+willior@users.noreply.github.com>
2020-08-27 16:04:37 -0400

    working on HP display

--- assets/Stats.gd
+++ assets/Stats.gd
@@ -4,8 +4,10 @@ export(int) var max_health = 4
 onready var health = max_health setget set_health
 
 signal no_health
+signal health_changed(value)
 
 func set_health(value):
 	health = value
+	emit_signal("health_changed", health) # every time the health is set, emits a signal "health_changed" along with an argument, our new health value
 	if health <= 0:
 		emit_signal("no_health")
