dc11d15bec9e7e231207b5ff74d95dd06b9460b9
willior <31899430+willior@users.noreply.github.com>
2020-08-26 21:48:09 -0400

    working on death effect
    enemies fall to the ground before exploding
    enemies now have a hit effect attached to their hurtbox

--- assets/World/Grass.gd
+++ assets/World/Grass.gd
@@ -1,11 +1,10 @@
 extends Node2D
 
-func create_grass_effect():
+const GrassEffect = preload("res://assets/Effects/GrassEffect.tscn") # preload the scene and store it in a constant
 
-	var GrassEffect = load("res://assets/Effects/GrassEffect.tscn") # load the scene and store it in a variable
+func create_grass_effect():
 	var grassEffect = GrassEffect.instance() # instance a packed scene of GrassEffect, storing it inside of its own variable, node grassEffect
-	var world = get_tree().current_scene # getting the very first scene, in this case World
-	world.add_child(grassEffect) # adding the grassEffect scene to the World scene
+	get_parent().add_child(grassEffect)
 	grassEffect.global_position = global_position # global_position is the position of Grass; sets the position of the grassEffect to the position of Grass
 
 func _on_Hurtbox_area_entered(area):
