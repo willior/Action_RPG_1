938b2ac4324e22804e59ff22097931604883bbfb
willior <31899430+willior@users.noreply.github.com>
2020-08-26 23:23:29 -0400

    fixing player hurtbox
    player has short invincibility after getting hurt

--- assets/CollisionBoxes/Hurtbox.gd
+++ assets/CollisionBoxes/Hurtbox.gd
@@ -28,3 +28,9 @@ func create_hit_effect():
 
 func _on_Timer_timeout():
 	self.invincible = false # if self prefixes invincible, calls the setter
+
+func _on_Hurtbox_invincibility_started():
+	set_deferred("monitorable", false)
+
+func _on_Hurtbox_invincibility_ended():
+	monitorable = true
--- assets/Player/Player.gd
+++ assets/Player/Player.gd
@@ -21,6 +21,7 @@ onready var animationPlayer = $AnimationPlayer # declaring animationPlayer to gi
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 onready var swordHitbox = $HitboxPivot/SwordHitbox
+onready var hurtbox = $Hurtbox
 
 func _ready():
 	stats.connect("no_health", self, "queue_free")
@@ -97,3 +98,5 @@ func roll_animation_finished():
 
 func _on_Hurtbox_area_entered(area):
 	stats.health -= 1
+	hurtbox.start_invincibility(0.5)
+	hurtbox.create_hit_effect()
