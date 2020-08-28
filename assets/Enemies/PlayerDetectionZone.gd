746da6a6a827d147f69e8e33911bcb7d7731c433
willior <31899430+willior@users.noreply.github.com>
2020-08-27 22:02:27 -0400

    enemies' detection zone gets bigger on detection, reverts if player can leave it

--- assets/Enemies/PlayerDetectionZone.gd
+++ assets/Enemies/PlayerDetectionZone.gd
@@ -2,11 +2,15 @@ extends Area2D
 
 var player = null
 
+onready var detectionZone = $CollisionShape2D
+
 func can_see_player():
 	return player != null
 
 func _on_PlayerDetectionZone_body_entered(body):
 	player = body
+	detectionZone.scale *= 2
 
 func _on_PlayerDetectionZone_body_exited(body):
 	player = null
+	detectionZone.scale /= 2
