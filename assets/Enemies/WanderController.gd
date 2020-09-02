0d8b81a2398c94f807af16770c7bc0e52e85e8bd
willior <31899430+willior@users.noreply.github.com>
2020-09-02 15:06:59 -0400

    wander state working properly

--- assets/Enemies/WanderController.gd
+++ assets/Enemies/WanderController.gd
@@ -7,6 +7,9 @@ onready var target_position = global_position # where the enemy will travel to
 
 onready var timer = $Timer
 
+func _ready():
+	update_target_position()
+
 func update_target_position():
 	var target_vector = Vector2(rand_range(-wander_range, wander_range), rand_range(-wander_range, wander_range))
 	target_position = start_position + target_vector # setting the target position RELATIVE to the start position
