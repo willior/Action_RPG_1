d951f6ca245f1c35aa759374f9217c09b87724e7
willior <31899430+willior@users.noreply.github.com>
2020-09-09 17:30:24 -0400

    tweaking exp display

--- assets/UI/ExpNotice.gd
+++ assets/UI/ExpNotice.gd
@@ -1,12 +1,21 @@
 extends Control
 
 onready var timer = $Timer
-onready var expText = $HBoxContainer/Label
+onready var expText = $Label
+onready var tween = $Tween
 
 var expDisplay
 
 func _ready():
 	expText.set_text(str(expDisplay) + " XP")
+	
+	# tween.interpolate_property(expText, "modulate", Color(1, 1, 1, 1), Color(1, 1, 1, 0), 1, Tween.TRANS_LINEAR, Tween.EASE_IN)
+	# tween.start()
+	
 	timer.start()
 	yield(timer, "timeout")
 	queue_free()
+
+func _process(delta):
+	rect_position.y -= 0.5
+	
