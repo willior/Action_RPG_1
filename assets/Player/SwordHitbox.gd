4f259dad26e49567c18561c4d9f6e83756e131ef
willior <31899430+willior@users.noreply.github.com>
2020-09-08 22:23:35 -0400

    strength goes up on level up

--- assets/Player/SwordHitbox.gd
+++ assets/Player/SwordHitbox.gd
@@ -1,8 +1,10 @@
-extends "res://assets/CollisionBoxes/Hitbox.gd"
+extends Area2D
+
+onready var damage = PlayerStats.strength setget set_strength
+var knockback_vector = Vector2.ZERO
 
 func set_strength(strength):
-	self.damage = strength
-	print(strength)
+	damage = PlayerStats.strength
 
 func _ready():
 	PlayerStats.connect("strength_changed", self, "set_strength")
