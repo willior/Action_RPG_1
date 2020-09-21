dc65906c3018bc9248ee8d6f41f44b4feb3a92aa
willior <31899430+willior@users.noreply.github.com>
2020-09-21 17:34:14 -0400

    NPC / dialog

--- assets/NPCs/Skeleton.gd
+++ assets/NPCs/Skeleton.gd
@@ -1,21 +1,33 @@
 extends Node2D
 
+const DialogBox = preload("res://assets/UI/Dialog.tscn")
+
 onready var player = get_parent().get_node("Player")
+onready var timer = $Timer
+onready var collision = $KinematicBody2D/Area2D/CollisionShape2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if (player.interactable == true && Input.is_action_pressed("attack") && timer.is_stopped()):
+		collision.disabled = true
+		var dialogBox = DialogBox.instance()
+		dialogBox.dialog = [
+			"Hello.",
+			"As you can see, I am a skeleton.",
+			"Unless, of course, you can't see.",
+			"In which case...",
+			"I'm still a skeleton."
+		]
+		get_node("/root/World/GUI").add_child(dialogBox)
+		timer.start()
 
 
 func _on_Area2D_area_entered(area):
 	print("area entered")
 	player.interactable = true
 
-
 func _on_Area2D_area_exited(area):
 	player.interactable = false
+	
+func _on_Timer_timeout():
+	collision.disabled = false
+	print('timeout')
--- assets/UI/Dialog.gd
+++ assets/UI/Dialog.gd
@@ -1,4 +1,4 @@
-extends RichTextLabel
+extends Polygon2D
 
 var dialog = [
 	"Hello.",
@@ -6,40 +6,40 @@ var dialog = [
 	"Whatever."
 	]
 	
-var dialogTest = [] setget set_dialog
-
 var dialog_index = 0
 
+onready var label = $RichTextLabel
+
 func _ready():
-	set_bbcode(dialog[dialog_index])
-	set_visible_characters(0)
-	set_process_input(true)
-	
-func set_dialog(value):
-	pass
+	# sets the text to that contained in the matching dialog_index array container
+	label.set_bbcode(dialog[dialog_index])
+	label.set_visible_characters(0)
+	label.set_process_input(true)
+	get_tree().paused = true
 	
 func _input(event):
 	if Input.is_action_just_pressed("attack"):
-		if (get_visible_characters() > get_total_character_count() && dialog_index >= dialog.size()-1):
-			get_parent().queue_free()
+		if (label.get_visible_characters() > label.get_total_character_count() && dialog_index >= dialog.size()-1):
+			queue_free()
+			get_tree().paused = false
 			
 		# if the amount of visible characters is above the total amount of characters in the current index:
-		elif get_visible_characters() > get_total_character_count():
+		elif label.get_visible_characters() > label.get_total_character_count():
 			# if the number of dialog_indexes in the array is less than the total amount in the array
 			if dialog_index < dialog.size()-1:
 				# advancing the dialog_index
 				dialog_index += 1
 				# setting the dialog
-				set_bbcode(dialog[dialog_index])
-				set_visible_characters(0)
+				label.set_bbcode(dialog[dialog_index])
+				label.set_visible_characters(0)
 		# if the amount of visible characters is less than the total amount of characters:
 		else:
 			# displays all the characters in the current dialog_index
-			set_visible_characters(get_total_character_count())
+			label.set_visible_characters(label.get_total_character_count())
 			
 	if Input.is_action_just_pressed("roll"):
-		print(get_visible_characters())
-		print(get_total_character_count())
+		print(label.get_visible_characters())
+		print(label.get_total_character_count())
 
 func _on_Timer_timeout():
-	set_visible_characters(get_visible_characters()+1)
+	label.set_visible_characters(label.get_visible_characters()+1)
