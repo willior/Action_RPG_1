7f0feaf024f3b42a23ba977e0437119e1421a158
willior <31899430+willior@users.noreply.github.com>
2020-09-22 14:19:03 -0400

    tweaking game over screen / added game over music

--- assets/NPCs/Skeleton.gd
+++ assets/NPCs/Skeleton.gd
@@ -12,7 +12,7 @@ var speaker = "Skeleton: "
 
 func _process(delta):
 	# skeleton talk
-	if (talkable && player.talking && Input.is_action_pressed("attack") && player.talkTimer.is_stopped()):
+	if (talkable && Input.is_action_pressed("attack") && player.talkTimer.is_stopped()):
 		# talkBox.disabled = true
 		var dialogBox = DialogBox.instance()
 		dialogBox.dialog = [
--- assets/UI/GameOver.gd
+++ assets/UI/GameOver.gd
@@ -5,27 +5,35 @@ onready var tween = $Tween
 onready var timer = $Timer
 
 func _ready():
+	$AudioStreamPlayer.play()
 	tween.interpolate_property(gameOverFlash,
 	"color",
 	Color(1, 1, 1, 1),
 	Color(1, 1, 1, 0),
-	5,
+	6,
 	Tween.TRANS_LINEAR,
 	Tween.EASE_IN
 	)
 	tween.start()
 	timer.start()
+	
 	yield(timer, "timeout")
 	tween.interpolate_property(gameOverFlash,
 	"color",
 	Color(0, 0, 0, 0),
 	Color(0, 0, 0, 1),
-	5,
+	6,
 	Tween.TRANS_LINEAR,
 	Tween.EASE_IN
 	)
 	tween.start()
+	timer.wait_time = 8
+	timer.start()
+	
+	yield(timer, "timeout")
+	$Label.visible = true
 
 func _process(delta):
 	if Input.is_action_just_pressed("start"):
 		prints('continue #', PlayerStats.continue_count)
+		queue_free()
