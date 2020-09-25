c7ef19812bdd71c8ebb74d04f3a368f5010654b9
willjeez <willgs@gmail.com>
2020-09-25 00:54:55 -0400

    cash UI displays

---
+++ assets/UI/CashUI.gd
@@ -0,0 +1,14 @@
+extends Control
+
+var currentCash setget set_cash
+
+onready var cashBox = $HBoxContainer/Label
+
+func set_cash(value):
+	currentCash = str(value)
+	cashBox.set_text(currentCash)
+	
+func _ready():
+	self.currentCash = PlayerStats.cash
+	
+	PlayerStats.connect("cash_changed", self, "set_cash")
