6bd5cfee5ec874e6b438b527cb1ae3ae10151c45
willjeez <willgs@gmail.com>
2020-10-04 15:22:20 -0400

    uploaded crow asset/created crow class
    i have copypasted the code from the bat class, and will be rewriting it.
    things that will differentiate from the bat:
    the crow has 3 distinct animations: takeoff, flight, and landing, in contrast to the bat's 1. when the crow is in the idle state, it should play the idle animation; when it wants to move from the idle state, it should takeoff and then play (loop) the flight animation until it wants to land, in which case it should play the land animation and re-enter the idle state.
    another thing i want to attempt is to give the crow a distinct 'attack' where its hitbox is active. ie, simply touching the crow should not damage you; it must perform its attack animation and hit you with it.

---
+++ assets/Enemies/BatStats.gd
@@ -0,0 +1,24 @@
+extends Node
+
+var max_health = 4 setget set_max_health
+var health = max_health setget set_health
+var experience_pool = 18
+
+signal no_health
+signal health_changed(value)
+signal max_health_changed(value)
+
+func set_max_health(value):
+	max_health = value
+	self.health = min(health, max_health)
+	emit_signal("max_health_changed", max_health)
+
+func set_health(value):
+	health = value
+	health = clamp(value, 0, max_health)
+	emit_signal("health_changed", health) # every time the health is set, emits a signal "health_changed" along with an argument, our new health value
+	if health <= 0:
+		emit_signal("no_health")
+
+func _ready():
+	self.health = max_health
