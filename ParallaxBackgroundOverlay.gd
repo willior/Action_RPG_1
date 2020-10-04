bd6bfe33c007fa5ce44694c4277504a73d884b44
willjeez <willgs@gmail.com>
2020-10-04 16:38:11 -0400

    Update ParallaxBackgroundOverlay.gd

--- ParallaxBackgroundOverlay.gd
+++ ParallaxBackgroundOverlay.gd
@@ -1,14 +1,13 @@
 extends ParallaxBackground
 
 onready var overlay = $ParallaxLayerOverlay
-onready var sprite = $ParallaxLayerOverlay/Sprite
-
-func _ready():
-	pass
 	
-func _process(delta):
-	print('processing')
+func _process(_delta):
 	overlay.motion_offset.x += 0.2
 	overlay.motion_offset.y += 0.2
-	#if overlay.motion_offset.x == 320: sprite.offset.x = 0
-	#if overlay.motion_offset.y == 180: sprite.offset.y = 0
+	if overlay.motion_offset.x == 320:
+		overlay.offset.x = 0
+		print('resetting x overlay')
+	if overlay.motion_offset.y == 180:
+		overlay.offset.y = 0
+		print('resetting y overlay')
