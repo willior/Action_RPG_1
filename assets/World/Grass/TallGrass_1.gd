c89721d257139f5d31d8c39e376fa56901be4a19
willjeez <willgs@gmail.com>
2020-10-08 19:29:54 -0400

    added 2 steps to grass cutting
    also randomized the flip_h property of grass, so ~half of them get flipped horizontally on ready.

--- assets/World/Grass/TallGrass_1.gd
+++ assets/World/Grass/TallGrass_1.gd
@@ -4,16 +4,29 @@ const EFFECT = preload("res://assets/Effects/TallGrass_Effect.tscn")
 
 onready var sprite = $AnimatedSprite
 
+var cutCount = 0
+
 func _ready():
+	randomize()
 	sprite.frame = randi() % 5
 	sprite.playing = true
+	sprite.flip_h = randi() % 2
 
 func create_effect():
-	sprite.play("Cut")
+	cutCount += 1
 	var effect = EFFECT.instance()
 	get_parent().add_child(effect)
 	effect.global_position = global_position
+	if sprite.flip_h:
+		effect.flip_h = true
+	if cutCount == 1:
+		sprite.play("Cut")
+	elif cutCount == 2:
+		sprite.play("CutComplete")
+		sprite.playing = false
+		effect.global_position.y += 8
 
 func _on_Hurtbox_area_entered(_area):
 	create_effect()
-	$Hurtbox.queue_free()
+	if cutCount == 2:
+		$Hurtbox.queue_free()
