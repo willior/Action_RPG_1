882d8f025537d0cbaa3e1e3e153b968417dcbf49
willjeez <willgs@gmail.com>
2020-10-10 22:58:51 -0400

    testing scene transitions

--- assets/Maps/Exit.gd
+++ assets/Maps/Exit.gd
@@ -1,5 +1,11 @@
 extends Area2D
 
+var tutorialScene = preload("res://assets/Maps/1-1_Forest.tscn")
+
 # monitoring for bodies on the Exits layer entering the area
 func _on_Exit_body_entered(_body):
 	print("exiting")
+	var map_tutorial = tutorialScene.instance()
+	# get_tree().get_root().add_child(map_tutorial)
+	# get_node("/root/World").free()
+	get_tree().change_scene("res://assets/Maps/1-1_Forest.tscn")
