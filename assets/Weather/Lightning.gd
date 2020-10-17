bb7115138afb080b561b47bc27fa7b791833d725
willjeez <willgs@gmail.com>
2020-10-17 03:03:09 -0400

    tuning lightning

--- assets/Weather/Lightning.gd
+++ assets/Weather/Lightning.gd
@@ -1,36 +1,18 @@
 extends Node2D
 
-export(int) var minTimeBetweenLightning = 10
-export(int) var maxTimeBetweenLightning = 20
+export(int) var minTimeBetweenLightning = 12
+export(int) var maxTimeBetweenLightning = 24
 var lightningTimer = 0
 var lightningPause = false
 
 func _ready():
 	randomize()
-	lightningTimer = rand_range(minTimeBetweenLightning, maxTimeBetweenLightning)
-	print(lightningTimer)
 
-#func _process(delta):
-#	if !lightningPause:
-#		lightningTimer -= delta
-#		if lightningTimer <= 0:
-#			print('playing lightning')
-#			$AnimationPlayer.play("Lightning1")
-#			yield(get_tree().create_timer(1), "timeout")
-#			print('timeout: playing thunder')
-#			$AudioStreamPlayer.play()
-#			lightningTimer = rand_range(minTimeBetweenLightning, maxTimeBetweenLightning)
-#			print(lightningTimer)
-			
 func lightning_strike():
+	$Timer.start(lightningTimer)
+	prints(str(lightningTimer) + " second til next lightning")
 	$AnimationPlayer.play("Lightning1")
-	print('playing lightning')
-	yield(get_tree().create_timer(2), "timeout")
-	print('timeout: playing thunder')
-	$AudioStreamPlayer.play()
 
 func _on_Timer_timeout():
-	lightning_strike()
 	lightningTimer = rand_range(minTimeBetweenLightning, maxTimeBetweenLightning)
-	print(lightningTimer)
-	$Timer.start(lightningTimer)
+	lightning_strike()
