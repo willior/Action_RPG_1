8fa23c594ce54d70280d6512628ca46c629744c3
willjeez <willgs@gmail.com>
2020-11-10 00:54:36 -0500

    began work on new dialog / added flowerpot

---
+++ assets/UI/DialogBox.gd
@@ -0,0 +1,14 @@
+extends Control
+
+onready var sprite = $Sprite
+onready var timer = $Timer
+
+func _ready():
+	sprite.position.x = 280
+	
+
+func next_indicator_animate():
+	
+
+func _on_Timer_timeout():
+	pass # Replace with function body.
