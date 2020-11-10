aa35bf75462891165347f95078abe75fb93d2e13
willjeez <willgs@gmail.com>
2020-11-10 12:40:12 -0500

    working on new DialogBox

--- assets/UI/DialogBox.gd
+++ assets/UI/DialogBox.gd
@@ -1,19 +1,30 @@
 extends Control
 
-onready var sprite = $Sprite
-onready var timer = $Timer
+var dialog = [
+	'Hello.',
+	'This is the second line of dialog. Number two.',
+	'This, here, is the third line of dialog. ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789XXXYYYZZZAAABBBCCCDDDASDFJKL;QWERTY###################'
+]
 
+var dialog_index = 0
+var finished = false
 var next_icon_modulator
 
 func _ready():
-	sprite.position.x = 280
+	load_dialog()
 	
-func next_indicator_animate():
-	pass
+func _input(_event):
+	if Input.is_action_just_pressed("attack") || Input.is_action_just_pressed("examine") || Input.is_action_just_pressed("item"):
+		load_dialog()
+	
+func load_dialog():
+	if dialog_index < dialog.size():
+		$RichTextLabel.bbcode_text = dialog[dialog_index]
+	dialog_index += 1
 
 func _on_Timer_timeout():
-	if sprite.position.x == 283:
+	if $Sprite.position.x == 283:
 		next_icon_modulator = -1
-	elif sprite.position.x == 280:
+	elif $Sprite.position.x == 280:
 		next_icon_modulator = 1
-	sprite.position.x += next_icon_modulator
+	$Sprite.position.x += next_icon_modulator
