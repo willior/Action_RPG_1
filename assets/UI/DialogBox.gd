39b2298dd3e1bbb115014c0ad765908b1797b8ab
willjeez <willgs@gmail.com>
2020-11-10 12:19:52 -0500

    working on new dialog box

--- assets/UI/DialogBox.gd
+++ assets/UI/DialogBox.gd
@@ -3,12 +3,17 @@ extends Control
 onready var sprite = $Sprite
 onready var timer = $Timer
 
+var next_icon_modulator
+
 func _ready():
 	sprite.position.x = 280
 	
-
 func next_indicator_animate():
-	
+	pass
 
 func _on_Timer_timeout():
-	pass # Replace with function body.
+	if sprite.position.x == 283:
+		next_icon_modulator = -1
+	elif sprite.position.x == 280:
+		next_icon_modulator = 1
+	sprite.position.x += next_icon_modulator
