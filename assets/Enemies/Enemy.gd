938d926eb94b98550818329ef04066de1a30666e
willjeez <willgs@gmail.com>
2021-01-12 15:47:02 -0500

    reverting stat values / beginning generic "Enemy" refactor project
    i really don't know the best way to go about refactoring enemies. i figure the final goal is to have a generic enemy class that has all the fundamental properties of what constitutes an "enemy" extended by specific enemy classes.
    that is kind of a daunting thing to do for me right now, so instead i think i will do it in "chunks", and make those common functions global with arguments. things like, "enemy_hit", i can have a generic version of the damage display logic like i do with damage calculation.
    i can still keep enemy-specific code in its class, but the idea is to move generic functions like displaying damage somewhere globally accessible.

---
+++ assets/Enemies/Enemy.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+# GENERIC ENEMY CLASS / REFACTOR PROJECT
+
+# common elements of the bat & crow go into a generic enemy class.
+# if their values need to be modified, export the appropriate variables.
+# the “bat” & “crow” classes, after their shared code has been refactored,
+# should extend this generic “enemy” class.
+
+func _ready():
+	pass
