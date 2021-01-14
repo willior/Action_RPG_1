bf40c01eb38e1539300edccd3eeb0e244bfd8371
willjeez <willgs@gmail.com>
2021-01-14 15:57:34 -0500

    getting rid of error messages

--- assets/Player/Pouch/Pouch.gd
+++ assets/Player/Pouch/Pouch.gd
@@ -2,9 +2,12 @@ extends Node
 
 export var _ingredients = Array() setget set_ingredients, get_ingredients
 
+# warning-ignore:unused_signal
 signal pouch_changed
+# warning-ignore:unused_signal
 signal ingredient_quantity_zero
 
+# warning-ignore:unused_argument
 func set_ingredients(new_ingredients):
 	pass
 	
