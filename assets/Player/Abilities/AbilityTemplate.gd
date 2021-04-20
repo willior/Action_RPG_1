25daf007c77e3e7017271726a020e2ef0e4ec617
willior <31899430+willior@users.noreply.github.com>
2021-04-20 11:27:15 -0400

    reworked spell animation into AnimationPlayer
    instead of using timers to delay the running of operations (like playing sounds, animations, screen shake, etc.), i've separated all ability events into discrete functions which can be sequenced on the Ability's AnimationPlayer (anim_name: Ability). this gives much more control over the design of abilities. any number of functions can be sequenced, over any period of time. you just have to write them beforehand. you could have a spell take 0.2s to cast and run 2 functions or you could have a spell take 20 minutes to cast and run 9999 functions.

--- assets/Player/Abilities/AbilityTemplate.gd
+++ assets/Player/Abilities/AbilityTemplate.gd
@@ -6,26 +6,27 @@ func _ready():
 	player.state = 9
 	player.animationTree.active = false
 	player.animationPlayer.play("Cast_1")
-	$AnimationPlayer.play("Ability")
-
-func ability_1():
+	yield(get_tree().create_timer(0.35), "timeout")
+	
 	$AudioStreamPlayer.play()
 	get_node("/root/World/Camera2D").decay = 0
 	get_node("/root/World/Camera2D").add_trauma(0.05)
-
-func ability_2():
+	$Timer.start()
+	yield($Timer, "timeout")
+	
 	get_node("/root/World/Camera2D").add_trauma(0.1)
-
-func ability_3():
+	$Timer.start()
+	yield($Timer, "timeout")
+	
 	get_node("/root/World/Camera2D").add_trauma(0.2)
-
-func ability_4():
+	$Timer.start()
+	yield($Timer, "timeout")
+	
 	get_node("/root/World/Camera2D").stop_shake()
 	get_node("/root/World/Camera2D").decay = 1
 	$CanvasLayer/White.flash()
 	$SpellHitbox.set_deferred("monitorable", true)
 	player.animationPlayer.play("Cast_2")
-
-func _on_AnimationPlayer_animation_finished(_anim_name):
-	print('ability over: deleting')
+	$Timer.start()
+	yield($Timer, "timeout")
 	queue_free()
