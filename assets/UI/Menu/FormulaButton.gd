6a3e5628e61cf60b99666ff4b437d501555b50b1
willior <31899430+willior@users.noreply.github.com>
2021-06-10 15:20:06 -0400

    fixed multiplayer pause screen controls
    pause screen controls should work as intended:
    whoever pauses the game controls inputs in the pause menu

--- assets/UI/Menu/FormulaButton.gd
+++ assets/UI/Menu/FormulaButton.gd
@@ -1,6 +1,7 @@
 extends Button
 const DialogBox = preload("res://assets/UI/Dialog/PopupDialogBox.tscn")
-onready var alchemyButton = get_parent().get_parent().get_parent().get_node("MenuPanel/Menu/ButtonAlchemy")
+onready var pauseMenu = get_parent().get_parent().get_parent()
+onready var alchemyButton = pauseMenu.get_node("MenuPanel/Menu/ButtonAlchemy")
 var description
 
 func _ready():
@@ -8,14 +9,14 @@ func _ready():
 
 func _on_Button_gui_input(event):
 	if event.is_action_pressed("ui_cancel"):
-		get_parent().get_parent().get_parent().hide_alchemy_display()
-		get_parent().get_parent().get_parent().enable_menu_focus()
+		pauseMenu.hide_alchemy_display()
+		pauseMenu.enable_menu_focus()
 		alchemyButton.grab_focus()
 	elif event.is_action_pressed("ui_select"):
-		get_parent().get_parent().get_parent().audio_menu_select()
-	elif event.is_action_pressed("start"):
-		get_parent().get_parent().get_parent().hide_alchemy_display()
-		get_parent().get_parent().get_parent().close_pause_menu()
+		pauseMenu.audio_menu_select()
+	elif event.is_action_pressed(pauseMenu.start):
+		pauseMenu.hide_alchemy_display()
+		pauseMenu.close_pause_menu()
 
 func _on_Button_pressed():
 	var dialogBox = DialogBox.instance()
--- assets/UI/Menu/IngredientButton.gd
+++ assets/UI/Menu/IngredientButton.gd
@@ -1,6 +1,7 @@
 extends Button
 const DialogBox = preload("res://assets/UI/Dialog/PopupDialogBox.tscn")
-onready var pouchButton = get_parent().get_parent().get_parent().get_parent().get_node("MenuPanel/Menu/ButtonPouch")
+onready var pauseMenu = get_parent().get_parent().get_parent().get_parent()
+onready var pouchButton = pauseMenu.get_node("MenuPanel/Menu/ButtonPouch")
 var description
 
 func _ready():
@@ -8,14 +9,14 @@ func _ready():
 
 func _on_Button_gui_input(event):
 	if event.is_action_pressed("ui_cancel"):
-		get_parent().get_parent().get_parent().get_parent().hide_pouch_display()
-		get_parent().get_parent().get_parent().get_parent().enable_menu_focus()
+		pauseMenu.hide_pouch_display()
+		pauseMenu.enable_menu_focus()
 		pouchButton.grab_focus()
 	elif event.is_action_pressed("ui_select"):
-		get_parent().get_parent().get_parent().get_parent().audio_menu_select()
-	elif event.is_action_pressed("start"):
-		get_parent().get_parent().get_parent().get_parent().hide_pouch_display()
-		get_parent().get_parent().get_parent().get_parent().close_pause_menu()
+		pauseMenu.audio_menu_select()
+	elif event.is_action_pressed(pauseMenu.start):
+		pauseMenu.hide_pouch_display()
+		pauseMenu.close_pause_menu()
 
 func _on_Button_pressed():
 	var dialogBox = DialogBox.instance()
