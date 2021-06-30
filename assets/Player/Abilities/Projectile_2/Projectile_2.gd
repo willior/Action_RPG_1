a44fd8aacc8714502dd8a094814062a88c15dd9a
willior <31899430+willior@users.noreply.github.com>
2021-06-30 16:33:37 -0400

    working on 2D physics for Projectile
    there are so many ways to handle physics, i will have to experiment with them all and find the one that feels the best.
    the projectile collides with world objects and deletes itself, but not when it hits enemies. this is because there are 2 separate layers for world collision and EnemyHurtbox collision. i think i need to re-do the projectile to account for both, as i think it'll be way too messy otherwise.
    i will also have to experiemnt with RigidBody2D, a node i have not used yet. it seems like it might be better/more efficient to use for a projectile.
    one of the issues is the the projectile has a Body and a Hitbox. if the body hits something (like a tree), it disappears, but when its Hitbox overlaps a Hurtbox, it behaves like a normal hitbox (ie., doesn't delete itself). maybe i will add a check on collision on the projectile itself to see if the thing collided with was an enemy or not, and generate/activate a hitbox on collision. now that i think about it, this might be useful for doing an angle-aimed AoE spell, like a grenade-type formula.
    i think more experimentation with different nodes, their methods, and 2D physics in general is in order.

--- assets/Player/Abilities/Projectile_2/Projectile_2.gd
+++ assets/Player/Abilities/Projectile_2/Projectile_2.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 
-const ACCELERATION = 320
+const ACCELERATION = 160
+var direction
 var target
 var speed = 16
 var velocity = Vector2.ZERO
@@ -8,16 +9,21 @@ var player
 
 func _ready():
 	print('projectile target pos: ', target)
-	# apply_impulse(Vector2.ZERO, target)
+	direction = global_position.direction_to(target) # gets the direction by grabbing the target position
+	# velocity = velocity.move_toward(direction, speed) # multiplies that by the speed argument
 
 func _process(delta):
-	accelerate_towards_point(target, delta)
-# warning-ignore:return_value_discarded
-	move_and_collide(velocity)
-
-func accelerate_towards_point(point, delta):
-	var direction = global_position.direction_to(point) # gets the direction by grabbing the target position, the point argument
-	velocity = velocity.move_toward(direction * speed, delta * ACCELERATION) # multiplies that by the speed argument
+	velocity = velocity.move_toward(direction*speed, delta*ACCELERATION)
+	if move_and_collide(velocity) != null:
+		print('projectile collided with something: ', move_and_collide(velocity), '; deleting projectile.')
+		queue_free()
 	if global_position.distance_to(target) <= 16:
 		print('projectile reached target. deleting projectile.')
 		queue_free()
+
+#func accelerate_towards_point(point):
+#	var direction = global_position.direction_to(point) # gets the direction by grabbing the target position, the point argument
+#	velocity = velocity.move_toward(direction, speed) # multiplies that by the speed argument
+#	if global_position.distance_to(target) <= 16:
+#		print('projectile reached target. deleting projectile.')
+#		queue_free()
